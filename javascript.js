7d8b3bff72e4ebac4c240d7fe35d3461ee4fc6b0
Lauren Carter <1146692526+laurencrtr@users.noreply.github.com>
2025-08-30 13:41:22 -0500

    Set box sizes for grid layout

--- javascript.js
+++ javascript.js
@@ -1,5 +1,18 @@
 const grid = document.querySelector(".grid");
+const clearButton = document.querySelector("#clear");
+const changeGridSizeButton = document.querySelector("#changeGridSize");
+const changeColorButton = document.querySelector("#changeColor");
+const eraserButton = document.querySelector("#eraser");
+// const changeColorButton = document.querySelector("#changeColor");
 
+let gridColor = 'white';
+let brushColor = 'black';
+
+function sayHi (){
+    alert("hi")
+}
+
+let currentBoxFunction = sayHi
 
 function getNumber() {
     let userNumber = prompt("Please enter a number 0-100. Your input must be a number.")
@@ -7,18 +20,24 @@ function getNumber() {
 }
 
 function createGrid(length) {
-    // totalBoxes = length * length
-    for (let i = 0; i < length; i++) {
-        newRow = document.createElement("div")
-        grid.appendChild(newRow)
-        for (let i = 0; i < length; i++) {
-            newBox = document.createElement("div")
-            newRow.appendChild(newBox)
-            console.log("grid added")
-        }  
+    let totalBoxes = length * length;
+    let boxWidth = (500/length) + 'px';
+
+    for (let i = 0; i < totalBoxes; i++) {
+        newBox = document.createElement("div")
+        newBox.style.width = boxWidth
+        newBox.style.height = boxWidth
+        newBox.style.backgroundColor = gridColor
+        newBox.textContent = i + 1
+        newBox.addEventListener("mouseEnter", currentBoxFunction);
+        grid.appendChild(newBox)
+
     }
 }
 
+
+
+
 // let gridSize = getNumber()
 // createGrid(gridSize)
-createGrid(3)
+createGrid(6)
